2996b072d0f254b37166c33c4da7e814fb5376e4
ShiryevIgorVl <ishirayev@gmail.com>
2020-09-02 11:41:32 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     static final int size = 10000000;
     static final int h = size / 2;
     static float[] arr = new float[size];
+    static float[] arr1 = new float[h];
+    static float[] arr2 = new float[h];
+
 
     public static float[] getArr() {
         for (int i = 0; i < arr.length; i++) {
@@ -17,12 +20,40 @@ public class Main {
         return arr;
     }
 
+
+
+
+
     public static void main(String[] args){
         getArr();
         long a = System.currentTimeMillis();
         newArr(arr);
         System.out.println(System.currentTimeMillis() - a);
 
+        long b = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               newArr(arr1);
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                newArr(arr2);
+            }
+        });
+        t1.start();
+        t2.start();
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - b);
+
+        System.out.println("End");
 
     }
+
+
 }
